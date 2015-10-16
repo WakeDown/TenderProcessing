f13f20d056a0c5777e8aaeea5e1ea6e4c3e051ec
WakeDown <wakedown.59@gmail.com>
2015-10-16 09:14:02 +0500

    merge

--- SpeCalcCode/SpeCalc/Models/UserHelper.cs
+++ SpeCalcCode/SpeCalc/Models/UserHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using SpeCalcDataAccessLayer.Models;
-using TenderProcessingDataAccessLayer.Models;
 
 namespace TenderProcessing.Models
 {
